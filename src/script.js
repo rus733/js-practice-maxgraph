e46e4d7d2d9e567b990270f5b0a73b06c0530ccf
rus733 <9282716617@mail.ru>
2022-01-29 12:03:31 +0300

    del old code

--- src/script.js
+++ src/script.js
@@ -1,39 +1,29 @@
 'use strict';
+
+//аккордеон с анимацией
 //console.log('script');
-// document.addEventListener('DOMContentLoaded', () => {
-//   const accordions = document.querySelectorAll('.accordion');
+document.addEventListener('DOMContentLoaded', () => {
+  const accordions = document.querySelectorAll('.accordion');
 
-//   accordions.forEach((el) => {
-//     el.addEventListener('click', (e) => {
-//       const self = e.currentTarget;
-//       const control = self.querySelector('.accordion__control');
-//       const content = self.querySelector('.accordion__content');
+  accordions.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      const self = e.currentTarget;
+      const control = self.querySelector('.accordion__control');
+      const content = self.querySelector('.accordion__content');
 
-//       self.classList.toggle('is-open');
+      self.classList.toggle('is-open');
 
-//       // если открыт аккордеон
-//       if (self.classList.contains('is-open')) {
-//         control.setAttribute('aria-expanded', true);
-//         content.setAttribute('aria-hidden', false);
-//         content.style.maxHeight = content.scrollHeight + 'px';
-//         //content.style.opacity = '1';
-//       } else {
-//         control.setAttribute('aria-expanded', false);
-//         content.setAttribute('aria-hidden', true);
-//         content.style.maxHeight = null;
-//       }
-//     });
-//   });
-// });
-class GraphAccordion {
-  constructor(selector, options) {
-    //
-    let defaultOptions = {
-      isOpen: () => {},
-      isClose: () => {},
-      speed: 300,
-    };
-    this.options = Object.assign(defaultOptions, options);
-    this.accordion = document.querySelector(selector);
-  }
-}
+      // если открыт аккордеон
+      if (self.classList.contains('is-open')) {
+        control.setAttribute('aria-expanded', true);
+        content.setAttribute('aria-hidden', false);
+        content.style.maxHeight = content.scrollHeight + 'px';
+        //content.style.opacity = '1';
+      } else {
+        control.setAttribute('aria-expanded', false);
+        content.setAttribute('aria-hidden', true);
+        content.style.maxHeight = null;
+      }
+    });
+  });
+});
