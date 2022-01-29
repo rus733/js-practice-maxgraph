db10c3d24fe50488c9de76b5c2fb522208822d3c
rus733 <9282716617@mail.ru>
2022-01-29 10:58:08 +0300

    runing is plugin

--- src/indexacc.js
+++ src/indexacc.js
@@ -1,3 +1,7 @@
 const accordion1 = new GraphAccordion('.accordion-1', {
   speed: 500,
 });
+
+const accordion2 = new GraphAccordion('.accordion-2', {
+  speed: 1500,
+});
--- src/script.js
+++ src/script.js
@@ -27,11 +27,13 @@
 // });
 class GraphAccordion {
   constructor(selector, options) {
-    //
     let defaultOptions = {
       isOpen: () => {},
       isClose: () => {},
       speed: 300,
+      // classes: {
+      //
+      // }
     };
     this.options = Object.assign(defaultOptions, options);
     this.accordion = document.querySelector(selector);
@@ -41,8 +43,37 @@ class GraphAccordion {
   }
 
   event() {
-    //
+    //console.log('event!');
+    //console.log(this.options.speed);
+    if (this.accordion) {
+      this.accordion.addEventListener('click', (e) => {
+        this.accordion.classList.toggle('open');
 
-    console.log('event! ');
+        if (this.accordion.classList.contains('open')) {
+          this.open();
+        } else {
+          this.close();
+        }
+      });
+    }
+  }
+
+  open() {
+    //
+    this.accordion.style.setProperty('--accordion-time', `${this.options.speed / 1000}s`);
+    this.accordion.classList.add('is-open');
+    this.control.setAttribute('aria-expanded', true);
+    this.content.setAttribute('aria-hidden', false);
+    this.content.style.maxHeight = this.content.scrollHeight + 'px';
+    this.options.isOpen(this);
+  }
+
+  close() {
+    //
+    this.accordion.classList.remove('is-open');
+    this.control.setAttribute('aria-expanded', false);
+    this.content.setAttribute('aria-hidden', true);
+    this.content.style.maxHeight = null;
+    this.options.isClose(this);
   }
 }
